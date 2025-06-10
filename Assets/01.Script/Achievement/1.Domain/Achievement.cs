3ff2a2354081b732967cdfad971ababa8b8ec246
wooing26 <104512612+wooing26@users.noreply.github.com>
2025-06-10 11:01:50 +0900

    Achievement

--- Assets/01.Script/Achievement/1.Domain/Achievement.cs
+++ Assets/01.Script/Achievement/1.Domain/Achievement.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.Tutorials.Core.Editor;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -14,64 +15,96 @@ public enum EAchievementCondition
 
 public class Achievement
 {
-     // 최종 목적: 자기서술형
+    // 최종 목적: 자기서술형
 
-     // 데이터
-     public readonly string ID; 
-     public readonly string Name;
-     public readonly string Description;
-     public readonly EAchievementCondition Condition;
-     public int GoalValue;
-     public ECurrencyType RewardCurrencyType;
-     public int RewardAmount;
+    // 데이터
+    public readonly string ID; 
+    public readonly string Name;
+    public readonly string Description;
+    public readonly EAchievementCondition Condition;
+    public int GoalValue;
+    public ECurrencyType RewardCurrencyType;
+    public int RewardAmount;
 
-     // 상태
-     private int _currentValue;
-     public int CurrentValue => _currentValue;
+    // 상태
+    private int _currentValue;
+    public int CurrentValue => _currentValue;
 
-     private bool _rewardClaimed;
-     public bool RewardClaimed => _rewardClaimed;
+    private bool _rewardClaimed;
+    public bool RewardClaimed => _rewardClaimed;
 
-     // 생성자
-     public Achievement(string id, string name, string description, EAchievementCondition condition, int goalValue, ECurrencyType rewardCurrencyType, int rewardAmount)
-     {
-          if (string.IsNullOrEmpty(id))
-          {
-               throw new Exception("업적 ID는 비어있을 수 없습니다.");
-          }
-          if (string.IsNullOrEmpty(name))
-          {
-               throw new Exception("업적 이름은 비어있을 수 없습니다.");
-          }
-          if (string.IsNullOrEmpty(description))
-          {
-               throw new Exception("업적 설명은 비어있을 수 없습니다.");
-          }
-          if (goalValue <= 0)
-          {
-               throw new Exception("업적 목표 값은 0보다 커야합니다.");
-          }
-          if (rewardAmount <= 0)
-          {
-               throw new Exception("업적 보상 값은 0보다 커야합니다.");
-          }
+    // 생성자
+    public Achievement(string id, string name, string description, EAchievementCondition condition, int goalValue, ECurrencyType rewardCurrencyType, int rewardAmount)
+    {
+        if (string.IsNullOrEmpty(id))
+        {
+            throw new Exception("업적 ID는 비어있을 수 없습니다.");
+        }
+        if (string.IsNullOrEmpty(name))
+        {
+            throw new Exception("업적 이름은 비어있을 수 없습니다.");
+        }
+        if (string.IsNullOrEmpty(description))
+        {
+            throw new Exception("업적 설명은 비어있을 수 없습니다.");
+        }
+        if (goalValue <= 0)
+        {
+            throw new Exception("업적 목표 값은 0보다 커야합니다.");
+        }
+        if (rewardAmount <= 0)
+        {
+            throw new Exception("업적 보상 값은 0보다 커야합니다.");
+        }
 
-          ID = id;
-          Name = name;
-          Description = description;
-          Condition = condition;
-          GoalValue = goalValue;
-          RewardCurrencyType = rewardCurrencyType;
-          RewardAmount = rewardAmount;
-     }
+        ID = id;
+        Name = name;
+        Description = description;
+        Condition = condition;
+        GoalValue = goalValue;
+        RewardCurrencyType = rewardCurrencyType;
+        RewardAmount = rewardAmount;
+    }
 
-     public void Increase(int value)
-     {
-          if (value <= 0)
-          {
-               throw new Exception("증가 값은 0보다 커야합니다.");
-          }
+    public Achievement(AchievementSO metadata)
+    {
+        if (string.IsNullOrEmpty(metadata.ID))
+        {
+            throw new Exception("업적 ID는 비어있을 수 없습니다.");
+        }
+        if (string.IsNullOrEmpty(metadata.Name))
+        {
+            throw new Exception("업적 이름은 비어있을 수 없습니다.");
+        }
+        if (string.IsNullOrEmpty(metadata.Description))
+        {
+            throw new Exception("업적 설명은 비어있을 수 없습니다.");
+        }
+        if (metadata.GoalValue <= 0)
+        {
+            throw new Exception("업적 목표 값은 0보다 커야합니다.");
+        }
+        if (metadata.RewardAmount <= 0)
+        {
+            throw new Exception("업적 보상 값은 0보다 커야합니다.");
+        }
+
+        ID = metadata.ID;
+        Name = metadata.Name;
+        Description = metadata.Description;
+        Condition = metadata.Condition;
+        GoalValue = metadata.GoalValue;
+        RewardCurrencyType = metadata.RewardCurrencyType;
+        RewardAmount = metadata.RewardAmount;
+    }
+
+    public void Increase(int value)
+    {
+        if (value <= 0)
+        {
+            throw new Exception("증가 값은 0보다 커야합니다.");
+        }
           
-          _currentValue += value;
-     }
+        _currentValue += value;
+    }
 }
--- Assets/01.Script/Achievement/3.Manager/AchievementManager.cs
+++ Assets/01.Script/Achievement/3.Manager/AchievementManager.cs
@@ -5,6 +5,9 @@ public class AchievementManager : MonoBehaviour
 {
     public static AchievementManager Instance;
 
+    [SerializeField]
+    private List<AchievementSO> _metaDatas;
+
     private List<Achievement> _achievements;
     
     private void Awake()
@@ -25,78 +28,13 @@ public class AchievementManager : MonoBehaviour
     private void Init()
     {
         // 초기화
-        
-        _achievements = new List<Achievement>
+
+        _achievements = new List<Achievement>();
+        foreach (var metaData in _metaDatas)
         {
-            new Achievement(
-                id: "ACH_MONEY_001",
-                name: "첫 수익",
-                description: "100 골드 획득",
-                condition: EAchievementCondition.GoldCollect,
-                goalValue: 100,
-                rewardCurrencyType: ECurrencyType.Gold,
-                rewardAmount: 300
-            ),
-    
-            new Achievement(
-                id: "ACH_MONEY_002",
-                name: "부자 되기 1단계",
-                description: "총 1000 골드 획득",
-                condition: EAchievementCondition.GoldCollect,
-                goalValue: 1000,
-                rewardCurrencyType: ECurrencyType.Diamond,
-                rewardAmount: 1
-            ),
-
-            new Achievement(
-                id: "ACH_KILL_DRONE_001",
-                name: "드론 헌터",
-                description: "드론 타입 몬스터 10마리 처치",
-                condition: EAchievementCondition.DronKillCount,
-                goalValue: 10,
-                rewardCurrencyType: ECurrencyType.Gold,
-                rewardAmount: 1000
-            ),
-
-            new Achievement(
-                id: "ACH_KILL_DRONE_002",
-                name: "드론 학살자",
-                description: "드론 타입 몬스터 50마리 처치",
-                condition: EAchievementCondition.DronKillCount,
-                goalValue: 100,
-                rewardCurrencyType: ECurrencyType.Diamond,
-                rewardAmount: 2
-            ),
-
-            new Achievement(
-                id: "ACH_KILL_BOSS_001",
-                name: "보스 저격수",
-                description: "보스 몬스터 1기 격추",
-                condition: EAchievementCondition.BossKillCount,
-                goalValue: 1,
-                rewardCurrencyType: ECurrencyType.Diamond,
-                rewardAmount: 3
-            ),
-
-            new Achievement(
-                id: "ACH_TIME_001",
-                name: "진득하게",
-                description: "플레이 누적 시간 10분 달성",
-                condition: EAchievementCondition.PlayTime,
-                goalValue: 600, // 초 단위라 가정
-                rewardCurrencyType: ECurrencyType.Diamond,
-                rewardAmount: 5
-            ),
-
-            new Achievement(
-                id: "ACH_HIDDEN_001",
-                name: "눈썰미",
-                description: "특정 위치의 숨겨진 장소",
-                condition: EAchievementCondition.Trigger,
-                goalValue: 777,
-                rewardCurrencyType: ECurrencyType.Diamond,
-                rewardAmount: 10
-            ),
-        };
+            Achievement achievement = new Achievement(metaData);
+            _achievements.Add(achievement);
+        }
+        
     }
 }
